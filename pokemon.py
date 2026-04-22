492fbe1064cd8a4b41f3275e221fbe190ac26b56
Keyla Paxtor <beatrizpax03@gmail.com>
2026-04-22 01:45:48 -0600

    Encapsulamiento

--- pokemon.py
+++ pokemon.py
@@ -8,3 +8,26 @@ class Pokemon(ABC):
         self._hp_maximo = hp_maximo
         self._energia_actual = energia_maxima
         self._energia_maxima = energia_maxima
+
+# Encapsulamiento
+    @property
+    def hp_actual(self):
+        return self._hp_actual
+
+    @hp_actual.setter
+    def hp_actual(self, puntos_hp):
+        if puntos_hp < 0:
+            self._hp_actual = 0
+        else:
+            self._hp_actual = puntos_hp
+
+    @property
+    def energia_actual(self):
+        return self._energia_actual
+
+    @energia_actual.setter
+    def energia_actual(self, puntos_energia):
+        if puntos_energia < 0:
+            self._energia_actual = 0
+        else:
+            self._energia_actual = puntos_energia
