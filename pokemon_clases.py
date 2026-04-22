15bd10d7636a0690be981f834783f0a4c5fe12b1
Keyla Paxtor <beatrizpax03@gmail.com>
2026-04-22 03:13:47 -0600

    Pokemon Eléctrico

--- pokemon_clases.py
+++ pokemon_clases.py
@@ -19,6 +19,7 @@ class PokemonAgua(Pokemon):
         self.energia_actual -= 15
         rival.hp_actual -= daño
         print(f'{self.nombre} hace {daño} de daño')
+        
 
 class PokemonFuego(Pokemon):
     def atacar(self, rival):
@@ -40,6 +41,7 @@ class PokemonFuego(Pokemon):
         rival.hp_actual -= daño
         print(f'{self.nombre} hace {daño} de daño')
 
+
 class PokemonPlanta(Pokemon):
     def atacar(self, rival):
         if self.energia_actual < 15:
@@ -55,6 +57,27 @@ class PokemonPlanta(Pokemon):
         if rival.defendiendo:
             daño //= 2
             rival.defendiendo = False
+
+        self.energia_actual -= 15
+        rival.hp_actual -= daño
+        print(f'{self.nombre} hace {daño} de daño')
+
+
+class PokemonElectrico(Pokemon):
+    def atacar(self, rival):
+        if self.energia_actual < 15:
+            print('No tienes energía suficiente para poder atacar.')
+            return
+
+        daño = 10
+
+        if rival.defendiendo:
+            daño //= 2
+            rival.defendiendo = False
+
+        if random.random() < 0.2:
+            rival.paralizado = True
+            print('El rival ha sido paralizado')
 
         self.energia_actual -= 15
         rival.hp_actual -= daño
