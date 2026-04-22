1cf06c5f46888bf7387683201c9a704072458231
Keyla Paxtor <beatrizpax03@gmail.com>
2026-04-22 03:12:38 -0600

    Pokemon Planta

--- pokemon_clases.py
+++ pokemon_clases.py
@@ -39,3 +39,23 @@ class PokemonFuego(Pokemon):
         self.energia_actual -= 15
         rival.hp_actual -= daño
         print(f'{self.nombre} hace {daño} de daño')
+
+class PokemonPlanta(Pokemon):
+    def atacar(self, rival):
+        if self.energia_actual < 15:
+            print('No tienes energía suficiente para poder atacar.')
+            return
+
+        daño = 10
+
+        if rival.tipo == 'Agua':
+            daño *= 2
+            print('Ataque efectivo')
+
+        if rival.defendiendo:
+            daño //= 2
+            rival.defendiendo = False
+
+        self.energia_actual -= 15
+        rival.hp_actual -= daño
+        print(f'{self.nombre} hace {daño} de daño')
